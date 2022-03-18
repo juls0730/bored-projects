685a0eb50d266fc6a22fcd3a643b5ae1ac94413a
Julian Garner <juliangarner240@gmail.com>
2022-03-17 22:47:31 -0500

    made terminal but still needs to be worked on

---
+++ terminal fake/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="terminal" id="terminal"> </div>
+</template>
+
+<script>
+document.onkeydown = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    if (charCode == 8 && document.getElementById("terminal").innerHTML !== "") {
+      document.getElementById("terminal").innerHTML = document.getElementById("terminal").innerHTML.substring(0, document.getElementById("terminal").innerHTML.length - 1)
+    } else if (charCode) {
+      console.log(charCode)
+      document.getElementById("terminal").innerHTML += String.fromCharCode(charCode);
+    }
+};
+</script>
+
+
+<style>
+@import './assets/base.css';
+</style>
